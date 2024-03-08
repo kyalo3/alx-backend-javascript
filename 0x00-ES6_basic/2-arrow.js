c1338aa0223d9b9d879df9fdb0dc60f9d062bf3b
Kyalo3 <kyalokimeu0@gmail.com>
2024-03-08 13:12:33 +0300

    3. Parameter defaults

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,6 +1,8 @@
 export default function getNeighborhoodsList() {
+  //function definition
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
+  //Arrow function definition
   this.addNeighborhood = (newNeighborhood) => {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
     return this.sanFranciscoNeighborhoods;
