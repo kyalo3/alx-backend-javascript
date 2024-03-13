953ab5c6d2ac045b54c9dd52c53bc4b22b49fec7
Kyalo3 <kyalokimeu0@gmail.com>
2024-03-13 11:52:11 +0300

    0. Creating an interface for a student

---
+++ 0x04-TypeScript/task_0/main.ts
@@ -0,0 +1,17 @@
+interface Student = {
+	firstName: string;
+	lastName: string;
+	location: string;
+}
+
+const student1: Student = {
+	firstName: "kyalo",
+	lastname: "kimeu",
+	location: "Nairobi",
+};
+
+const student2: Student = {
+	firstName: "John",
+	lastname: "Munene",
+	location: "Nairobi",
+};
