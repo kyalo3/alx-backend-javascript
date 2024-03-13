d18505129ed940a2e55fb7642dd7d9ae6a03301b
Kyalo3 <kyalokimeu0@gmail.com>
2024-03-13 11:20:58 +0300

    new changes

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,19 @@
+const obj = [
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' },
+];
+
+function getListStudentIds(students) {
+  // Check if the argument is an array
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  // Use the map function to extract the ids
+  return students.map((student) => student.id);
+}
+
+export default getListStudentIds;
 function getStudentsByLocation(students, city) {
   const studentsList = students.filter((students) => students.city);
 }
