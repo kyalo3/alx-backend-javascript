c96dac8b537e42715b8812439c6e6afc49fb8e4d
Kyalo3 <kyalokimeu0@gmail.com>
2024-03-13 12:43:19 +0300

    2. Extending the Teacher class

--- 0x04-TypeScript/task_1/main.ts
+++ 0x04-TypeScript/task_1/main.ts
@@ -7,10 +7,29 @@ interface Teacher {
 	contract: boolean;
 }
 
-const teacher3: Teacher {
+interface Directors {
+	firstName: string;
+	lastName: string;
+	fullTimeEmployee: boolean;
+	yearsOfExperience: number;
+	location: string;
+	contract: boolean;
+}
+
+const teacher3: Teacher = {
 	firstName: 'John',
 	fullTimeEmployee: false,
 	lastName: 'Doe',
 	location: 'London',
 	contract: false,
 };
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
