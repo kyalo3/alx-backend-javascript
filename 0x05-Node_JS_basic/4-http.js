d65a1825bb89ba3ad87ca1b45ebd104d54f0bc23
Kyalo3 <kyalokimeu0@gmail.com>
2024-04-30 16:13:17 +0300

    Create a small HTTP server using Nodes HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -7,7 +7,7 @@ const port = 1245;
 // Create the HTTP server
 const app = http.createServer((req, res) => {
   // Set the response header
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.setHeader(200, { 'Content-Type': 'text/plain' });
 
   // Send the response body
   res.end('Hello Holberton School!\n');
