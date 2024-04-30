43764b7558e2185976f98956623853641f6f813c
Kyalo3 <kyalokimeu0@gmail.com>
2024-04-30 15:40:18 +0300

    servver setup

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,8 +1,10 @@
-var http = require('http')
+const { createServer } = require('node:http');
+
+const port = 1245;
 
 //create a server object:
-http.createServer(function (req, res) {
+const server = createServer((req, res) => {
   res.write('Hello Holberton School!'); //write a response
 //to the client
   res.end(); //end the response
-}).listen(1245); // the server object listens on port 1245
+}).listen(1245)
