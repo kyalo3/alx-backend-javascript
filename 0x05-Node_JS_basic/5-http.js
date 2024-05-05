c8605c82315417c4a7e021eefae5f18eaffd91ea
Kyalo3 <kyalokimeu0@gmail.com>
2024-05-05 21:36:16 +0300

    Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,51 +1,36 @@
-// 5-http.js
-
 const http = require('http');
-const fs = require('fs');
 
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
+const DATABASE = args[0];
+
+const hostname = '127.0.0.1';
 const port = 1245;
 
-// Function to read CSV file asynchronously
-const readCSVFileAsync = (filename, callback) => {
-  fs.readFile(filename, 'utf8', (err, data) => {
-    if (err) {
-      callback(err, null);
-      return;
-    }
-    // Split the CSV data by lines and filter out empty lines
-    const lines = data.split('\n').filter((line) => line.trim() !== '');
-    callback(null, lines);
-  });
-};
-
-// Create the HTTP server
-const app = http.createServer((req, res) => {
-  // Set the response header
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
 
-  // Handling requests based on URL path
-  if (req.url === '/') {
-    res.statusCode = 200;
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    res.statusCode = 200;
-    // Read the CSV file asynchronously
-    readCSVFileAsync('database.csv', (err, data) => {
-      if (err) {
-        console.error('Error reading file:', err);
-        res.end('Error reading file');
-        return;
-      }
-      // Send the list of students as response
-      res.end(`This is the list of our students\n${data.join('\n')}`);
-    });
-  } else {
-    res.statusCode = 404;
-    res.end('Page Not Found');
+  const { url } = req;
+
+  if (url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const students = await countStudents(DATABASE);
+      res.end(`${students.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
+    }
   }
+  res.statusCode = 404;
+  res.end();
 });
 
-// Listen on port 1245
-app.listen(port);
+app.listen(port, hostname, () => {
+  //   console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 module.exports = app;
