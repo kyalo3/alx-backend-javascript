f35fe3728c2491dc8a7172be4b13624d003b604e
Kyalo3 <kyalokimeu0@gmail.com>
2024-05-24 13:42:59 +0300

    new task files

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,13 +1,13 @@
 export default class Car {
-    constructor(brand, motor, color) {
-      if (typeof (brand) === 'string' && typeof (motor) === 'string' && typeof (color) === 'string') {
-        this._brand = brand;
-        this._motor = motor;
-        this._color = color;
-      }
-    }
-  
-    cloneCar() {
-      return new this.constructor(this._brand, this._motor, this._color);
+  constructor(brand, motor, color) {
+    if (typeof (brand) === 'string' && typeof (motor) === 'string' && typeof (color) === 'string') {
+      this._brand = brand;
+      this._motor = motor;
+      this._color = color;
     }
   }
+
+  cloneCar() {
+    return new this.constructor(this._brand, this._motor, this._color);
+  }
+}
