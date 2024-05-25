91ca87a7aaff607dd61971b3606d6f76f36942da
Kyalo3 <kyalokimeu0@gmail.com>
2024-05-25 10:24:40 +0300

    filter

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,19 +1,5 @@
-const obj = [
-  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-  { id: 5, firstName: 'Serena', location: 'San Francisco' },
-];
+import getListStudents from './0-get_list_students';
 
-function getListStudentIds(students) {
-  // Check if the argument is an array
-  if (!Array.isArray(students)) {
-    return [];
-  }
-
-  // Use the map function to extract the ids
-  return students.map((student) => student.id);
-}
-
-export default getListStudentIds;
-function getStudentsByLocation(students, city) {
-  const studentsList = students.filter((students) => students.city);
+export default function getStudentsByLocation(student = getListStudents(), city) {
+  return student.filter((el) => el.location === city);
 }
